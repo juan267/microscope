099c7beb775315333abd700eb04fbdb307297bd0
juan267 <j.gomez267@gmail.com>
2015-12-13 18:36:41 -0500

    Added basic posts list template and static data

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,15 @@
+var postsData = [
+  {
+    title: "Introducinf telescope",
+    url: "http://monkeybaby.co"
+  },
+  {
+    title: "meteor",
+    url: "http://meteor.com"
+  }
+];
+
+Template.postsList.helpers({
+  posts: postsData
+})
+
