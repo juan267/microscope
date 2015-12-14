596eec1ed12e1bb42a03cb6d0e94a470a24946c8
juan267 <j.gomez267@gmail.com>
2015-12-13 19:22:25 -0500

    remove autpoblich and set up basic publication

---
+++ client/main.js
@@ -0,0 +1,3 @@
+Meteor.susbcribe('posts');
+
+
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,15 +1,17 @@
-var postsData = [
-  {
-    title: "Introducinf telescope",
-    url: "http://monkeybaby.co"
-  },
-  {
-    title: "meteor",
-    url: "http://meteor.com"
-  }
-];
+// var postsData = [
+//   {
+//     title: "Introducinf telescope",
+//     url: "http://monkeybaby.co"
+//   },
+//   {
+//     title: "meteor",
+//     url: "http://meteor.com"
+//   }
+// ];
 
 Template.postsList.helpers({
-  posts: postsData
+  posts: function(){
+    return Posts.find();
+  }
 })
 
