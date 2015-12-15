eb6f56a1ec9fc112aa5a51cd8730bd56ccd61fb2
juan267 <j.gomez267@gmail.com>
2015-12-14 19:03:30 -0500

    enforcing url uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,13 +1,25 @@
 Template.postSubmit.events({
-  'submit form': function(e){
-    e.preventDefault();
+ 'submit form': function(e) {
+   e.preventDefault();
 
-    var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
-    };
+   var post = {
+     url: $(e.target).find('[name=url]').val(),
+     title: $(e.target).find('[name=title]').val()
+   };
+    // post._id = Posts.insert(post);
+    // Router.go('postPage', post)
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post)
-  }
-})
+   Meteor.call('postInsert', post, function(error, result) {
+     // display the error to the user and abort
+     if (error){
+       return alert(error.reason);
+     }
+
+     if(result.postExists){
+        alert("This link has already been posted");
+     }
+
+     Router.go('postPage', {_id: result._id});
+   });
+ }
+});
