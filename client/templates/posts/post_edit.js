8a85f84c93c1528355711a7764742c9748ffd39d
juan267 <j.gomez267@gmail.com>
2015-12-14 21:49:33 -0500

    validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -9,6 +9,11 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
+    var errors = validatePost(postProperties)
+    if (errors.title || errors.url){
+      return Session.set('postEditErrors', errors)
+    }
+
     Posts.update(currentPostId, {$set: postProperties}, function(error){
       if(error){
         throwError(error.reason)
@@ -29,3 +34,16 @@ Template.postEdit.events({
   }
 })
 
+Template.postEdit.onCreated(function(){
+  Session.set('postEditErrors', {})
+});
+
+
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field]
+  },
+  errorClass: function(field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+})
